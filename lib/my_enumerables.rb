184a135ce26b35a8bff53b69f9917bb19dc99e2f
Rohan James <rohanjames9282@gmail.com>
2022-09-03 14:52:01 +0530

    Add my_each

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,6 @@
 module Enumerable
   # Your code goes here
+
 end
 
 # You will first have to define my_each
@@ -8,4 +9,8 @@ end
 # to this method
 class Array
   # Define my_each here
+  def my_each
+    return to_enum(:my_each, arr) unless block_given?
+     yield
+    end
 end
