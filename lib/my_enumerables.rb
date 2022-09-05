06b1a21dce95cb92fa782a78a0f94b5c9983b393
Rohan James <rohanjames9282@gmail.com>
2022-09-05 13:13:45 +0530

    Remove unnecessary comments

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -120,12 +120,7 @@ module Enumerable
 
 end
 
-# You will first have to define my_each
-# on the Array class. Methods defined in
-# your enumerable module will have access
-# to this method
 class Array
-  # Define my_each here
   def my_each
     return to_enum(:my_each) unless block_given?
      for i in self
